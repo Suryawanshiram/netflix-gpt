7d45a5ef08ba4ae7d33d4084d4485a25b4f02094
Suryawanshiram <suryawanshiram2@gmail.com>
2024-02-18 00:19:17 +0530

    Validation for email and password added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 // import Header from "./Header";
+import {checkValidData}  from ".././utils/validate"
 
 const Login = () => {
   const [isSignForm, setIsSignInForm] = useState(true)
 
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
   const toggleSignInForm = () => {
-       setIsSignInForm(!isSignForm)
+    setIsSignInForm(!isSignForm)
+  
   }; 
+
+  const handleButtonClick = () => { 
+    // validate form data
+    console.log(email.current.value);
+    console.log(password.current.value);
+    const message = checkValidData(email.current.value ,password.current.value)
+    console.log(message);
+  }
+
   return <div>
     {/* <Header /> */}
     
@@ -16,15 +31,15 @@ const Login = () => {
       <img className="w-44 object-contain text-sm" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
     </div>
 
-      <form className="absolute ml-56 mt-40 mx-auto items-center justify-center gap-5 p-20 bg-black right-0 left-0 flex flex-col w-fit">
-        <h1 className="text-white font-bold text-3xl items-center justify-center text-justify mx-auto">{isSignForm ? "SignIn" : "Sign Up"}</h1>
+      <form onSubmit={(e) => e.preventDefault()} className="absolute ml-56 mt-40 mx-auto items-center justify-center gap-5 p-20 bg-black right-0 left-0 flex flex-col w-3/6 bg-opacity-70 rounded-md">
+        <h1 className="text-white font-bold text-3xl items-center justify-center text-justify mx-auto"> {isSignForm ? "SignIn" : "Sign Up " }</h1>
         {!isSignForm && (
-          <input type="text" placeholder="Full Name" className="px-4 py-2 m-3 rounded-md bg-gray-300" />
+          <input type="text" placeholder="Full Name" className="px-4 py-2 m-3 rounded-md " />
         )}
-        <input type="text" placeholder="Email address" className="px-4 py-2 m-3 rounded-md bg-gray-300" />
-           <input type="password" placeholder="Password" className="px-4 py-2 rounded-md m-3 bg-slate-300" />
-          <button onClick={toggleSignInForm} className="mx-auto w-full px-4 items-center justify-center text-center py-2 rounded-md  bg-red-500 gap-5">{isSignForm ? "Sign In" : "Sign Up"}</button>
-         <p className="text-white  py-4 "onClick={toggleSignInForm}>New to Netflix? Sign Up Now</p>
+        <input ref={email} type="text" placeholder="Email address" className="px-4 py-2 m-3 rounded-md bg-gray-40" />
+           <input ref={password} type="password" placeholder="Password" className="px-4 py-2 rounded-md m-3 bg-slate-300" />
+        <button onClick={ handleButtonClick} className="mx-auto w-full px-4 items-center justify-center text-center py-2 rounded-md  bg-red-500 gap-5">{isSignForm ? "Sign In" : "Sign Up"}</button>
+         <p className="text-white  py-4" onClick={toggleSignInForm}>New to Netflix? Sign Up Now</p>
         </form>
       </div>
     <div className="w-full flex">
---
+++ src/utils/validate.js
@@ -0,0 +1,9 @@
+export const checkValidData = (email,password) => {
+    const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(password);
+
+    if (!isEmailValid) return "Email Id is not  Valid";
+    if (!isPasswordValid) return "Password is not valid";
+
+    return null;
+}
