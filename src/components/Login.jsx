881f7c174238510082f6101badb52664dfc3a2c4
Suryawanshiram <suryawanshiram2@gmail.com>
2024-02-18 23:37:53 +0530

    adding set up of firebase and its hosting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,51 +1,87 @@
 import { useRef, useState } from "react";
 // import Header from "./Header";
-import {checkValidData}  from ".././utils/validate"
+import { checkValidData } from ".././utils/validate";
 
 const Login = () => {
-  const [isSignForm, setIsSignInForm] = useState(true)
+  const [isSignForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
 
   const toggleSignInForm = () => {
-    setIsSignInForm(!isSignForm)
-  
-  }; 
+    setIsSignInForm(!isSignForm);
+  };
 
-  const handleButtonClick = () => { 
+  const handleButtonClick = () => {
     // validate form data
-    console.log(email.current.value);
-    console.log(password.current.value);
-    const message = checkValidData(email.current.value ,password.current.value)
-    console.log(message);
-  }
+    // console.log(email.current.value);
+    // console.log(password.current.value);
 
-  return <div>
-    {/* <Header /> */}
-    
-    <div className="w-full flex justify-center items-between gap-5">
-      <h1>Sign In</h1>
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black w-full">
-      <img className="w-44 object-contain text-sm" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
-    </div>
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
 
-      <form onSubmit={(e) => e.preventDefault()} className="absolute ml-56 mt-40 mx-auto items-center justify-center gap-5 p-20 bg-black right-0 left-0 flex flex-col w-3/6 bg-opacity-70 rounded-md">
-        <h1 className="text-white font-bold text-3xl items-center justify-center text-justify mx-auto"> {isSignForm ? "SignIn" : "Sign Up " }</h1>
-        {!isSignForm && (
-          <input type="text" placeholder="Full Name" className="px-4 py-2 m-3 rounded-md " />
-        )}
-        <input ref={email} type="text" placeholder="Email address" className="px-4 py-2 m-3 rounded-md bg-gray-40" />
-           <input ref={password} type="password" placeholder="Password" className="px-4 py-2 rounded-md m-3 bg-slate-300" />
-        <button onClick={ handleButtonClick} className="mx-auto w-full px-4 items-center justify-center text-center py-2 rounded-md  bg-red-500 gap-5">{isSignForm ? "Sign In" : "Sign Up"}</button>
-         <p className="text-white  py-4" onClick={toggleSignInForm}>New to Netflix? Sign Up Now</p>
+  return (
+    <div>
+      {/* <Header /> */}
+
+      <div className="w-full flex justify-center items-between gap-5">
+        <h1>Sign In</h1>
+        <div className="absolute px-8 py-2 bg-gradient-to-b from-black w-full">
+          <img
+            className="w-44 object-contain text-sm"
+            src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+            alt="logo"
+          />
+        </div>
+
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="absolute ml-56 mt-40 mx-auto items-center justify-center gap-5 p-20 bg-black right-0 left-0 flex flex-col w-3/6 bg-opacity-70 rounded-md"
+        >
+          <h1 className="text-white font-bold text-3xl items-center justify-center text-justify mx-auto">
+            {" "}
+            {isSignForm ? "SignIn" : "Sign Up "}
+          </h1>
+          {!isSignForm && (
+            <input
+              ref={name}
+              type="text"
+              placeholder="Full Name"
+              className="px-4 py-2 m-3 rounded-md "
+            />
+          )}
+          <input
+            ref={email}
+            type="text"
+            placeholder="Email address"
+            className="px-4 py-2 m-3 rounded-md bg-gray-40"
+          />
+          <input
+            ref={password}
+            type="password"
+            placeholder="Password"
+            className="px-4 py-2 rounded-md m-3 bg-slate-300"
+          />
+          <p className="text-red-500 font-bold text-lg p-2">{errorMessage}</p>
+          <button
+            onClick={handleButtonClick}
+            className="mx-auto w-3/4 px-4 items-center justify-center text-center py-2 rounded-md  bg-red-500 gap-5"
+          >
+            {isSignForm ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="text-white  py-4" onClick={toggleSignInForm}>
+            New to Netflix? Sign Up Now
+          </p>
         </form>
       </div>
-    <div className="w-full flex">
-    <img src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg"/>
+      <div className="w-full flex">
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDF9-sY4kay08QWhKijOJSu-GDCsvCPFtA",
+  authDomain: "netflixgpt-1a3ca.firebaseapp.com",
+  projectId: "netflixgpt-1a3ca",
+  storageBucket: "netflixgpt-1a3ca.appspot.com",
+  messagingSenderId: "299121686778",
+  appId: "1:299121686778:web:c3ea6b62e3d5f5c547f143",
+  measurementId: "G-CMCZRC0HKG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
